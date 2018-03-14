0ec62c204d0930134f7434d06acb611480743387
olaven <olavsundfoer@gmail.com>
2018-03-14 15:46:56 +0100

    working on (and stuffing up) JSON-implementation. Renamed the two versions to 'basic' and 'extra'

--- V18_1_extra/src/main/java/arbeidskrav/MeterArchive.java
+++ V18_1_extra/src/main/java/arbeidskrav/MeterArchive.java
@@ -1,11 +1,18 @@
+package arbeidskrav;
+
 import java.util.ArrayList;
 
 public class MeterArchive {
 
     private ArrayList<Meter> meters;
+    private FileManager fileManager = new FileManager();
+    private JsonManager jsonManager = new JsonManager();
+
+    private String PATH_TO_ARCHIVE = "./src/main/java/arbeidskrav/data/meterArchive.json";
+    private String ARCHIVE_METER_KEY = "meters"; 
 
     public MeterArchive() {
-        meters = new ArrayList<Meter>();
+        meters = getMeters();
     }
 
     /**
@@ -14,6 +21,7 @@ public class MeterArchive {
      * @param meter - the meter to be added 
      */
     public boolean add(Meter meter) {
+        //TODO: if content is valid JSON, add a new Meter. If not, replace content with valid json of Meter 
         if (fetch(meter.getIdentification()) == null) {
             meters.add(meter);
             return true;
@@ -27,27 +35,23 @@ public class MeterArchive {
      * @param identification - identification of the string to be removed 
      */
     public boolean remove(String identification) {
-        /*
-        for (Meter meter : meters) {
-            if (meter.getIdentification() == identification) {
-                meters.remove(meter);
-                return true;
-            }
-        }
-        return false;
-        */
-
-        Meter meter = fetch(identification); 
-        if (meter == null) return false; 
-        meters.remove(meter); 
-        return true; 
+        Meter meter = fetch(identification);
+        if (meter == null)
+            return false;
+        meters.remove(meter);
+        /**
+         * IMPLEMENTING: use JSON file 
+         */
+        String without = jsonManager.removeFromJsonArray(meter, fileManager.getContent(PATH_TO_ARCHIVE), ARCHIVE_METER_KEY); 
+        fileManager.replaceContent(PATH_TO_ARCHIVE, without); 
+        return true;
     }
 
     /**
      * Returns specified meter, if in the archive 
      */
     public Meter fetch(String identification) {
-        for (Meter meter : meters) {
+        for (Meter meter : getMeters()) {
             if (meter.getIdentification() == identification) {
                 return meter;
             }
@@ -93,5 +97,36 @@ public class MeterArchive {
         for (Meter meter : meters) {
             System.out.println(meter.toString());
         }
+    }
+
+    /**
+     * returns true if the archive has valid JSON in it
+     */
+    public boolean validArchive() {
+        return jsonManager.isValidJson(fileManager.getContent(PATH_TO_ARCHIVE));
+    }
+
+    /**
+     * returns the meters registered as 
+     * objects in an ArrayList 
+     */
+    private ArrayList<Meter> getMeters() {
+        String archive = fileManager.getContent(PATH_TO_ARCHIVE); 
+        ArrayList<Object> objectList = jsonManager.getFromJsonArray(archive, ARCHIVE_METER_KEY); 
+        ArrayList<Meter> meterList = new ArrayList<Meter>(); 
+        
+        try {
+            for(Object meter : objectList){
+                meterList.add((Meter) meter); 
+            }
+        } catch (Exception e) {
+            System.out.println(
+                "Somehow, something other than meters have been stored"
+                +"\nMeters is emptied of the non-meter content"
+            ); 
+            return new ArrayList<Meter>(); 
+        }
+
+        return meterList; 
     }
 }
