22b020e5a945a66057e205a1d30d1503e9b66bde
olaven <olavsundfoer@gmail.com>
2018-04-06 16:31:31 +0200

    auto formatting + went over comments

--- V18_2/source/Question.java
+++ V18_2/source/Question.java
@@ -6,43 +6,62 @@
  * Answeres are stored in lowercase 
  */
 public class Question {
-    private String text; 
-    private String answer; 
+    private String text;
+    private String answer;
     private String imagePath;
-    private boolean correct; 
+    private boolean correct;
 
-    //This is the constructor I intend to use 
+    /**
+     * This is the constructor I intend to use 
+     */
     public Question(String text, String answer, String imagePath) {
         this(text, answer, imagePath, false);
     }
-    //this constructor may come in handy at some point NOTE: remove if not
-    public Question(String text, String answer, String imagePath, boolean correct){
-        setText(text); 
-        setAnswer(answer.toLowerCase()); 
-        setImagePath(imagePath); 
+
+    /**
+     * this constructor may come in handy at some point (NOTE: Remove if not)
+     */
+    private Question(String text, String answer, String imagePath, boolean correct) {
+        setText(text);
+        setAnswer(answer.toLowerCase());
+        setImagePath(imagePath);
         setCorrect(correct); //this option may come in handy. Remove if not 
     }
 
     //get 
-    public String getText(){return text;}
-    public String getAnswer(){return answer;}
-    public String getImagePath(){return imagePath;}
-    public boolean getCorrect(){return correct;}
+    public String getText() {
+        return text;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public boolean getCorrect() {
+        return correct;
+    }
 
     //set
-    public void setText(String text){
-        if(text.length() > 0)
-            this.text = text; 
+    public void setText(String text) {
+        if (text.length() > 0)
+            this.text = text;
     }
-    public void setAnswer(String answer){
-        if(answer.length() > 0)
-            this.answer = answer; 
+
+    public void setAnswer(String answer) {
+        if (answer.length() > 0)
+            this.answer = answer;
     }
-    public void setImagePath(String imagePath){
-        this.imagePath = imagePath; 
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
     }
-    public void setCorrect(boolean correct){
-        this.correct = correct; 
+
+    public void setCorrect(boolean correct) {
+        this.correct = correct;
     }
-    
+
 }
--- V18_2/source/Quiz.java
+++ V18_2/source/Quiz.java
@@ -8,83 +8,97 @@ import javafx.scene.image.Image;
  */
 public class Quiz {
 
-    private String title; 
+    private String title;
 
-    private ArrayList<Question> questions; 
+    private ArrayList<Question> questions;
     //index of current question 
-    private int currentQuestionIndex; 
+    private int currentQuestionIndex;
 
-    public Quiz(String title){
-        this(title, 0); 
+    /**
+     * constructor 
+     * @param title title of the quiz 
+     */
+    public Quiz(String title) {
+        this(title, 0);
     }
-    public Quiz(String title, int currentQuestionIndex){
-        questions = new ArrayList<Question>(); 
-        setTitle(title); 
-        setCurrentQuestionIndex(currentQuestionIndex); 
+
+    /**
+    * constructor 
+    * @param title title of the quiz 
+    * @param currentQuestionIndex the index of first question (default 0)
+    */
+    public Quiz(String title, int currentQuestionIndex) {
+        questions = new ArrayList<Question>();
+        setTitle(title);
+        setCurrentQuestionIndex(currentQuestionIndex);
     }
 
     /**
      * Validate answer, assign true or false to .getCorrect()
+     * @param submittedAnswer the answer being handled 
      */
-    public void handleAnswer(String submittedAnswer){
-        Question question = questions.get(getCurrentQuestionIndex()); 
-        submittedAnswer = submittedAnswer.toLowerCase(); 
+    public void handleAnswer(String submittedAnswer) {
+        Question question = questions.get(getCurrentQuestionIndex());
+        submittedAnswer = submittedAnswer.toLowerCase();
         String correctAnswer = question.getAnswer();
-        
-        if(submittedAnswer.equals(correctAnswer)){
-            question.setCorrect(true); 
+
+        if (submittedAnswer.equals(correctAnswer)) {
+            question.setCorrect(true);
         } else {
-            question.setCorrect(false); 
+            question.setCorrect(false);
         }
     }
 
     /**
      * Change question to the next one 
-     * Returns true if mroe questions, false if not 
+     * Returns true if more questions, false if not 
      */
-    public boolean nextQuestion(){
+    public boolean nextQuestion() {
         //if no exception, there is a new question 
         try {
-            questions.get(getCurrentQuestionIndex() + 1); 
-            System.out.println("there is a new question: " + getCurrentQuestionIndex()); 
+            questions.get(getCurrentQuestionIndex() + 1);
         } catch (IndexOutOfBoundsException e) {
-            System.out.println("no new question"); 
-            return false; 
+            return false;
         }
-        setCurrentQuestionIndex(getCurrentQuestionIndex() + 1); 
-        return true;  
+        setCurrentQuestionIndex(getCurrentQuestionIndex() + 1);
+        return true;
     }
-    
+
     /**
      * Adds questions to the list 
      * Could be replaed with reading from a 
      * file at some point 
      */
-    public void addTestQuestions(){
-        questions.add(new Question("Name the capital?", "Oslo", "file:../images/norway.png")); 
+    public void addTestQuestions() {
+        questions.add(new Question("Name the capital?", "Oslo", "file:../images/norway.png"));
         questions.add(new Question("Name the capital?", "Copenhagen", "file:../images/denmark.png"));
         questions.add(new Question("Name the capital?", "Stockholm", "file:../images/sweden.png"));
         questions.add(new Question("Name the capital?", "Haag", "file:../images/netherlands.png"));
     }
-    
+
     //get 
-    public String getTitle(){
-        return title; 
+    public String getTitle() {
+        return title;
     }
-    public Question getCurrentQuestion(){
-        return questions.get(getCurrentQuestionIndex()); 
+
+    public Question getCurrentQuestion() {
+        return questions.get(getCurrentQuestionIndex());
     }
-    private int getCurrentQuestionIndex(){
-        return currentQuestionIndex; 
+
+    private int getCurrentQuestionIndex() {
+        return currentQuestionIndex;
     }
+
     public int getQuestionCount() {
         return questions.size();
     }
+
     //set
-    public void setTitle(String title){
-        this.title = title; 
+    public void setTitle(String title) {
+        this.title = title;
     }
-    public void setCurrentQuestionIndex(int currentQuestionIndex){
-        this.currentQuestionIndex = currentQuestionIndex; 
+
+    public void setCurrentQuestionIndex(int currentQuestionIndex) {
+        this.currentQuestionIndex = currentQuestionIndex;
     }
 }
